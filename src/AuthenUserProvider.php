21ebed675a80dc23a93ef51cdbe935c2a678e8be
Mior Muhammad Zaki <crynobone@gmail.com>
2021-08-01 20:51:28 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/AuthenUserProvider.php
+++ src/AuthenUserProvider.php
@@ -11,7 +11,7 @@ class AuthenUserProvider extends EloquentUserProvider
     /**
      * Retrieve a user by the given credentials.
      *
-     * @param  array  $credentials
+     * @param  array<string,string>  $credentials
      *
      * @return \Illuminate\Contracts\Auth\Authenticatable|null
      */
@@ -47,7 +47,7 @@ class AuthenUserProvider extends EloquentUserProvider
      * Validate a user against the given credentials.
      *
      * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
-     * @param  array  $credentials
+     * @param  array<string,string>  $credentials
      *
      * @return bool
      */
