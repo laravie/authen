71a0c8a829186391b0c4ea0314128fb64a59a219
Mior Muhammad Zaki <crynobone@gmail.com>
2022-12-18 11:53:12 +0800

    wip

--- tests/Factories/UserFactory.php
+++ tests/Factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends \Orchestra\Testbench\Factories\UserFactory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'username' => $this->faker->unique()->userName(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
