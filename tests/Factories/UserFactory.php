6ea2f79aae56dee8b9d5b3e5c68ff1f08fc543a9
Mior Muhammad Zaki <crynobone@gmail.com>
2023-02-08 11:19:12 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Factories/UserFactory.php
+++ tests/Factories/UserFactory.php
@@ -2,34 +2,66 @@
 
 namespace Laravie\Authen\Tests\Factories;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Str;
 use Laravie\Authen\Tests\Fixtures\User;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Laravie\Authen\Tests\Fixtures\User>
- */
-class UserFactory extends \Orchestra\Testbench\Factories\UserFactory
-{
+if (version_compare(Application::VERSION, '10.0', '>=')) {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
+     * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Laravie\Authen\Tests\Fixtures\User>
      */
-    protected $model = User::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
+    class UserFactory extends \Orchestra\Testbench\Factories\UserFactory
     {
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'username' => $this->faker->unique()->userName(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
+        /**
+         * The name of the factory's corresponding model.
+         *
+         * @var string
+         */
+        protected $model = User::class;
+
+        /**
+         * Define the model's default state.
+         *
+         * @return array
+         */
+        public function definition(): array
+        {
+            return [
+                'name' => $this->faker->name(),
+                'email' => $this->faker->unique()->safeEmail(),
+                'username' => $this->faker->unique()->userName(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ];
+        }
+    }
+} else {
+    /**
+     * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Laravie\Authen\Tests\Fixtures\User>
+     */
+    class UserFactory extends \Orchestra\Testbench\Factories\UserFactory
+    {
+        /**
+         * The name of the factory's corresponding model.
+         *
+         * @var string
+         */
+        protected $model = User::class;
+
+        /**
+         * Define the model's default state.
+         *
+         * @return array
+         */
+        public function definition()
+        {
+            return [
+                'name' => $this->faker->name(),
+                'email' => $this->faker->unique()->safeEmail(),
+                'username' => $this->faker->unique()->userName(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ];
+        }
     }
 }
